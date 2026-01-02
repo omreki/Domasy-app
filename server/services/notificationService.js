d8e3608b9be5059dc9669cf3ff4a2b86d8586285
omreki <omrekilimited@gmail.com>
2026-01-03 02:32:39 +0300

    Fix supabase client import in NotificationService

--- server/services/notificationService.js
+++ server/services/notificationService.js
@@ -1,4 +1,4 @@
-const { supabase } = require('../config/supabase');
+const supabase = require('../config/supabase');
 
 class NotificationService {
     static async createNotification(userId, { title, message, type = 'info', link = null }) {
